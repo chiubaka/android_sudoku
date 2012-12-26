c5406552134b12ebc449e4a57f80b81bee423cbd
Daniel Chiu <daniechiu@gmail.com>
2012-12-25 21:00:59 -0800

    Add about dialog box and settings.

--- src/com/chiubaka/sudoku/Sudoku.java
+++ src/com/chiubaka/sudoku/Sudoku.java
@@ -3,20 +3,56 @@ package com.chiubaka.sudoku;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class Sudoku extends Activity {
+public class Sudoku extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sudoku);
+		// Set up click listeners for all the buttons
+		View continueButton = findViewById(R.id.continue_button);
+		continueButton.setOnClickListener(this);
+		View newButton = findViewById(R.id.new_button);
+		newButton.setOnClickListener(this);
+		View aboutButton = findViewById(R.id.about_button);
+		aboutButton.setOnClickListener(this);
+		View exitButton = findViewById(R.id.exit_button);
+		exitButton.setOnClickListener(this);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_sudoku, menu);
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.settings:
+			startActivity(new Intent(this, Prefs.class));
+			return true;
+		// More items go here (if any) ...
+		}
+		return false;
+	}
+	
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.about_button:
+			Intent i = new Intent(this, About.class);
+			startActivity(i);
+			break;
+		// More buttons go here (if any) ...
+		}
 	}
 
 }
