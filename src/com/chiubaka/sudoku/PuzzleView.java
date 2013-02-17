8589d54935a5155dbef5e5be88a73f3b1ffcd60f
Daniel Chiu <daniechiu@gmail.com>
2013-02-16 20:47:48 -0800

    Add hint coloring.

--- src/com/chiubaka/sudoku/PuzzleView.java
+++ src/com/chiubaka/sudoku/PuzzleView.java
@@ -93,6 +93,23 @@ public class PuzzleView extends View {
 			}
 		}
 		// Draw the hints...
+		// Pick a hint color based on #moves left
+		Paint hint = new Paint();
+		int c[] = { getResources().getColor(R.color.puzzle_hint_0),
+				getResources().getColor(R.color.puzzle_hint_1),
+				getResources().getColor(R.color.puzzle_hint_2)
+		};
+		Rect r = new Rect();
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				int movesleft = 9 - game.getUsedTiles(i, j).length;
+				if (movesleft < c.length) {
+					getRect(i, j, r);
+					hint.setColor(c[movesleft]);
+					canvas.drawRect(r,  hint);
+				}
+			}
+		}
 		// Draw the selection...
 		Log.d(TAG, "selRect=" + selRect);
 		Paint selected = new Paint();
