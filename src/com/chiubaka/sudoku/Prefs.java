44c03e9f4f0f4f8ce193fea2cb170ce354bfa31f
Daniel Chiu <daniechiu@gmail.com>
2013-02-19 14:11:24 -0800

    Implement preferences for background music and displaying of hints.

--- src/com/chiubaka/sudoku/Prefs.java
+++ src/com/chiubaka/sudoku/Prefs.java
@@ -1,12 +1,31 @@
 package com.chiubaka.sudoku;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 public class Prefs extends PreferenceActivity {
+	// Option names and default values
+	private static final String OPT_MUSIC = "music";
+	private static final boolean OPT_MUSIC_DEF = true;
+	private static final String OPT_HINTS = "hints";
+	private static final boolean OPT_HINTS_DEF = true;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.settings);
+	}
+	
+	/** Get the current value of the music option */
+	public static boolean getMusic(Context context) {
+		return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(OPT_MUSIC, OPT_MUSIC_DEF);
+	}
+	
+	// fuck
+	/** Get the current value of the hints option */
+	public static boolean getHints(Context context) {
+		return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(OPT_HINTS, OPT_HINTS_DEF);
 	}
 }
